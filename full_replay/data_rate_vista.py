ade810808f25ae814a1880556c969680a078cb0f
Gurveer Sohal <gurveersohal433@gmail.com>
2023-09-27 12:38:47 -0600

    change

--- full_replay/data_rate_vista.py
+++ full_replay/data_rate_vista.py
@@ -888,6 +888,7 @@ def data_rate_vista_automated(
         plt_images_dir = os.path.join(os.getcwd(), "plt_images")
         if not os.path.exists(plt_images_dir):
             os.makedirs(plt_images_dir)
+        
         for i in range(numScenes):
             if i == 0:
                 #ORIGINAL PLOT
@@ -907,8 +908,37 @@ def data_rate_vista_automated(
                 offset = (i - 1) * 0.7
                 ax4_new.spines['right'].set_position(('outward', offset * 100))
 
+        fig, ax = plt.subplots()
+        t = np.linspace(0, 3, 40)
+        g = -9.81
+        v0 = 12
+        z = g * t**2 / 2 + v0 * t
+
+        v02 = 5
+        z2 = g * t**2 / 2 + v02 * t
+
+        scat = ax.scatter(t[0], z[0], c="b", s=5, label=f'v0 = {v0} m/s')
+        line2 = ax.plot(t[0], z2[0], label=f'v0 = {v02} m/s')[0]
+        ax.set(xlim=[0, 3], ylim=[-4, 10], xlabel='Time [s]', ylabel='Z [m]')
+        ax.legend()
+
+
+        def update(frame):
+            # for each frame, update the data stored on each artist.
+            x = t[:frame]
+            y = z[:frame]
+            # update the scatter plot:
+            data = np.stack([x, y]).T
+            scat.set_offsets(data)
+            # update the line plot:
+            line2.set_xdata(t[:frame])
+            line2.set_ydata(z2[:frame])
+            return (scat, line2)
+
+    ani = animation.FuncAnimation(fig=fig, func=update, frames=40, interval=30)
+    plt.show()
     # Datarate graphs
-    if enable_graphical:
+    if not enable_graphical:
         if enable_regression:
             # Need to add main title and axis titles
             ''' # BANDAID FIX  
