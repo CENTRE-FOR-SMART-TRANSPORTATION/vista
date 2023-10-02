25d70635ac0d73aa7f562dbfd49648ba0ee72e22
Gurveer Sohal <gurveersohal433@gmail.com>
2023-10-02 09:40:20 -0600

    change

--- full_replay/data_rate_vista_excel.py
+++ full_replay/data_rate_vista_excel.py
@@ -680,11 +680,11 @@ def data_rate_vista_automated(
         df.to_excel(filename, 'sheet1', index=False)
 
     # Get data rate plots for simple method
-    filename = f'{os.path.basename(os.path.normpath(path_to_scenes))}_occupancy'
+    filename = f'{os.path.basename(os.path.normpath(vistaoutput_path))}_occupancy'
     saveToExcel(filename, outmatrix_count, an_data_rate2,
                 an_data_rate2_ave, 'Simple method datarate', 'Data rate for occupancy count', 'distance (m)',
                 'Atomic norm Data rate', True)
-    filename = f'{os.path.basename(os.path.normpath(path_to_scenes))}_volume'
+    filename = f'{os.path.basename(os.path.normpath(vistaoutput_path))}_volume'
     saveToExcel(outmatrix_count, an_data_rate,
                 an_data_rate_ave, 'Volume method datarate', 'Data rate for volumetric method', 'distance (m)',
                 'Atomic norm Data rate', True)
